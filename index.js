56c8125ced75b1d9d07aa6add81b01b042ea51fa
AinazZ <ziyazova.aynaz@mail.ru>
2021-02-15 21:50:07 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Import a library to help create component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// Create a component
+const App = () => (
+    <Text>Some Text</Text>
+);
+
+// Render it to device
+AppRegistry.registerComponent('albums', () => App);
